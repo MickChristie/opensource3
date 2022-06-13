6426ecc3de9af827d350fe75347b222bdc7a89c3
MickChristie <80893253+MickChristie@users.noreply.github.com>
2022-06-14 03:32:00 +0900

    Update main.py
    Added final 2 functions

--- main.py
+++ main.py
@@ -28,7 +28,28 @@ async def addNum(anewnum: str, count: int = 1):
 @app1.get("numbers/sum")
 async def sumNum():
   if len(listofnum) >= 1:
-    print("The sum of all numbers entered so far is: " sum(listofnum))
+    print("The sum of all numbers entered so far is: ", sum(listofnum))
   else:
     print("No values entered")
   return{"result:111"}
+
+@app1.get("numbers/average")
+async def averageNum():
+  if len(listofnum) >= 1:
+    print("The average of all numbers entered so far is: ", sum(listofnum)/len(listofnum))
+  else:
+    print("No values entered")
+  return{"result:111"}
+
+@app1.get("numbers/stddev")
+async def stddevNum():
+  if len(listofnum) >= 1:
+    n = len(listofnum)
+    mean= sum(listofnum)/len(listofnum)
+    var = sum((x - mean)**2 for x in ls) / n
+    standev = var ** 0.5
+    print("The standard deviation of all numbers entered so far is: ",(standev) )
+  else:
+    print("No values entered")
+  return{"result:111"}
+
